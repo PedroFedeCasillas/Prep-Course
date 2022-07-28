a325397c8eb6fffdc6e482508570b661ff388d1b
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2022-07-27 22:36:49 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,19 +4,19 @@
 const nuevaString = "hola mundo";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = (2);
+const nuevoNum = 2;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -25,18 +25,22 @@ const nuevoModulo = 21 % 5 === null;
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-function devolverString(str) {
+function devolverString(nombre, apellido) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return nombre + apellido;
 }
+devolverString ("Pedro" , "Casillas");
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  var result= x+y;
+  return result;
 }
+var result=suma(4,9);
+console.log(result);
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor

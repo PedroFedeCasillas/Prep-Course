a20f81b04069be41cfa45dd113aa0f42bad9bab2
Pedro Fede Casillas <pedrofedecasillas2@gmail.com>
2022-09-25 15:01:39 -0300

    10 realizado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+     var matriz = [];
+     for(var propiedad in objeto){
+      matriz.push([propiedad, objeto[propiedad]]);
+     }
+     return matriz;
 }
 
 
@@ -18,6 +24,16 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+
+  var cantCaracteres = {};
+  for(var i = 0; i < string.length; i++){
+    if(cantCaracteres.hasOwnProperty(string[i])){
+      cantCaracteres[string[i]]++;
+    } else{
+      cantCaracteres[string[i]]=1;
+    }
+  }
+  return cantCaracteres;
 }
 
 
@@ -26,7 +42,19 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+
+     var mayusculas = "";
+     var minusculas = "";
+     for(var i = 0; i < s.length; i++){
+      if(s[i] !== s[i].toUpperCase()){
+        minusculas += s[i];
+      } else{
+        mayusculas += s[i];
+      }
+     }
+     return mayusculas + minusculas;
 }
+
 
 
 function asAmirror(str) {
@@ -35,6 +63,13 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+
+    var palabrasSeparadas = str.split(" ");
+    var fraseInvertida = [];
+    for(var i = 0; i < palabrasSeparadas.length; i++){
+      fraseInvertida.push(palabrasSeparadas[i].split("").reverse().join(""));
+    }
+    return fraseInvertida.join(" ");
 } 
 
 
@@ -43,6 +78,13 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+
+  var numeroString = numero.toString();
+  for(var i = 0; i < numeroString.length / 2; i++){
+    if(!(numeroString[i] === numeroString[numeroString.length - 1 - i]))
+    return "No es capicua";
+  }
+  return "Es capicua";
 }
 
 
@@ -50,6 +92,14 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+
+   var palabra = "";
+   for(var i = 0; i < cadena.length; i++){
+    if(cadena[i] !== "a" && cadena[i] !== "b" && cadena[i] !== "c"){
+      palabra += cadena[i];
+    }
+   }
+   return palabra;
 }
 
 
@@ -57,6 +107,21 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+
+  do {
+    var cambios = false;
+    for(var i = 0; i < arr.length - 1; i++){
+      if(arr[i].length > arr[i + 1].length){
+        var store = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = store;
+        cambios = true;
+      }
+    }
+    
+  } while (cambios);
+  return arr;
+
 }
 
 
@@ -65,7 +130,19 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí 
+  
+    var nuevoArreglo = [];
+    for (let i = 0; i < arreglo1.length; i++) {
+      var element = arreglo1[i];
+      for(var j = 0; j < arreglo2.length; j++){
+        if(element === arreglo2[j]){
+          nuevoArreglo.push(element);
+        }
+      }
+      
+    }
+    return nuevoArreglo;
 }
 
 
